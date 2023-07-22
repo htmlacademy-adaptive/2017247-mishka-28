eee77a43fd7f53a76ddc35e710590cdf52039a32
Viktoriya <tori.7979@mail.ru>
2023-07-22 19:52:20 +0300

    Делает сборку проекта и разметку всех страниц

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+let navMain = document.querySelector('.main-navigation');
+let navToggle = document.querySelector('.main-navigation__toggle');
+
+navMain.classList.remove('main-navigation--nojs');
+navMain.classList.add('main-navigation--closed');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+  } else {
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
+  }
+});
